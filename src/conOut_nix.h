e0d3be85b49b5e1c7804687be3ab3a518c3537b1
p-mcgowan <mickeymcgowan@shaw.ca>
2015-01-06 15:40:58 -0800

    output port

--- src/conOut_nix.h
+++ src/conOut_nix.h
@@ -1,7 +1,14 @@
-#define CLEAR "\x1b[2J"
-#define CC_CLEAR "\x1b[0m" // Console Colour CLEAR
 // FG Colours
-#define BLACK "\x1b[30m"
+#define BLACK 30
+#define RED 31
+#define GREEN 32
+#define YELLOW 33
+#define BLUE 34
+#define MAGENTA 35
+#define CYAN 36
+#define WHITE 37
+/*#define BLACK "\x1b[30m"
+#define BGC_BLACK "\x1b[40m"
 #define RED "\x1b[31m"
 #define GREEN "\x1b[32m"
 #define YELLOW "\x1b[33m"
@@ -18,39 +25,30 @@
 #define BGC_MAGENTA "\x1b[45m"
 #define BGC_CYAN "\x1b[46m"
 #define BGC_WHITE "\x1b[47m"
-
-void gotoXY(int x, int y) {
-  printf("%c[%d;%df",0x1B,y,x);
-}
-void clrs(void) {
+*/
+void clrsc(int c, int b) {  
+  setCB(c, b);
   printf("\x1b[2J");
 }
-void setCB(int ch, int bg) {
-  std::string c = "\033[1;" + std::to_string(ch) + 'm';
-  std::string b = "\033[7;" + std::to_string(bg) + 'm';
-// \033[7;#m - Colours the background according to #
-}
-void clrsc(int ch, int bg) {
-}
-void SetColour(int colour){
-//#define RED "\033[1;31m"
-  std::string c = "\033[1;" + std::to_string(colour) + 'm';
-  std::cout << c;
-}
-void clrsc(int c, int b) {  
+void clrs() {
   printf("\x1b[2J");
 }
 void gotoxy(unsigned int x, unsigned int y){
   printf("\x1b[%d;%df", y, x);
 }
 void setC(int c) {
+  char col[11] = {'\\',}
   printf("%s",Colour);
 }
-void textbackground(char Colour[11]) {
-char col[11];
-strcpy(col,Colour);
-col[2]='4'; // The given Colour will be fg Colour. Replace ’3′ with ’4′.
-printf("%s",col);
+void setCB(int c, int b) {
+  //#define BLACK "\x1b[30m"
+//#define BGC_BLACK "\x1b[40m"
+  setC(c);
+
+  char col[11] = {'\\', 'x', '1', 'b', '[', 40 'm'
+  strcpy(col,Colour);
+  col[2]='4';  // replace fg with bg
+  printf("%s",col);
 }
 /*
 Escape code Use
@@ -61,4 +59,7 @@ Escape code Use
 \x1b[;km  Where k is the colour (text colours are from 30 to 37 and background colours are from 40 to 47). For the colour codes, see Table 2.
 \x1b[5m Blinks slowly
 \x1b[6m Blinks rapidly
+void gotoXY(int x, int y) {
+  printf("%c[%d;%df",0x1B,y,x);
+}
 */
--- src/conOut_win.h
+++ src/conOut_win.h
@@ -1,9 +1,9 @@
 /*****************************************************************
- * WinConsole.c: Demonstrates using the Win32 console API to
- * manipulate console output.
- * Program by: NickDMax
- http://www.dreamincode.net/code/snippet921.htm
- *****************************************************************/
+* WinConsole.c: Demonstrates using the Win32 console API to
+* manipulate console output.
+* Program by: NickDMax
+http://www.dreamincode.net/code/snippet921.htm
+*****************************************************************/
 
 #include <windows.h>
 #include <stdio.h>
@@ -13,93 +13,58 @@
 
 //This will clear the console while setting the forground and
 //  background colors.
-void clrsc(int ForgC, int BackC)
-{
-     WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
-     //Get the handle to the current output buffer...
-     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-     //This is used to reset the carat/cursor to the top left.
-     COORD coord = {0, 0};
-     //A return value... indicating how many chars were written
-     //   not used but we need to capture this since it will be
-     //   written anyway (passing NULL causes an access violation).
-     DWORD count;
-
-     //This is a structure containing all of the console info
-     // it is used here to find the size of the console.
-     CONSOLE_SCREEN_BUFFER_INFO csbi;
-     //Here we will set the current color
-     SetConsoleTextAttribute(hStdOut, wColor);
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
-     {
-          //This fills the buffer with a given character (in this case 32=space).
-          FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
-
-          FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count );
-          //This will set our cursor position for the next print statement.
-          SetConsoleCursorPosition(hStdOut, coord);
-     }
-     return;
+void clrsc(int ForgC, int BackC) {
+  WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
+  HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD coord = {0, 0};
+  DWORD count;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  SetConsoleTextAttribute(hStdOut, wColor);
+  if(GetConsoleScreenBufferInfo(hStdOut, &csbi)) {
+    FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X *   csbi.dwSize.Y, coord, &count);
+    FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.  dwSize.X * csbi.dwSize.Y, coord, &count );
+    SetConsoleCursorPosition(hStdOut, coord);
+  }
+  return;
 }
 
 //This will clear the console.
-void clrs()
-{
-     //Get the handle to the current output buffer...
-     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-     //This is used to reset the carat/cursor to the top left.
-     COORD coord = {0, 0};
-     //A return value... indicating how many chars were written
-     //   not used but we need to capture this since it will be
-     //   written anyway (passing NULL causes an access violation).
-     DWORD count;
-     //This is a structure containing all of the console info
-     // it is used here to find the size of the console.
-     CONSOLE_SCREEN_BUFFER_INFO csbi;
-     //Here we will set the current color
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
-     {
-          //This fills the buffer with a given character (in this case 32=space).
-          FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
-          FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count );
-          //This will set our cursor position for the next print statement.
-          SetConsoleCursorPosition(hStdOut, coord);
-     }
-     return;
+void clrs() {
+  HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD coord = {0, 0};
+  DWORD count;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  if(GetConsoleScreenBufferInfo(hStdOut, &csbi)) {
+     FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+     FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count );
+     SetConsoleCursorPosition(hStdOut, coord);
+  }
+  return;
 }
 
 //This will set the position of the cursor
-void gotoxy(int x, int y)
-{
-     //Initialize the coordinates
-     COORD coord = {x, y};
-     //Set the position
-     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-     return;
+void gotoxy(int x, int y) {
+  COORD coord = {x, y};
+  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+  return;
 }
 
 //This will set the forground color for printing in a console window.
-void setC(int ForgC)
-{
-     WORD wColor;
-     //We will need this handle to get the current background attribute
-     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-     CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-     //We use csbi for the wAttributes word.
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
-     {
-        //Mask out all but the background attribute, and add in the forgournd color
-          wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
-          SetConsoleTextAttribute(hStdOut, wColor);
-     }
-     return;
+void setC(int ForgC) {
+  WORD wColor;
+  HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  if(GetConsoleScreenBufferInfo(hStdOut, &csbi)) {
+     wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+     SetConsoleTextAttribute(hStdOut, wColor);
+  }
+  return;
 }
 
 //This will set the forground and background color for printing in a console window.
 void setCB(int ForgC, int BackC)
 {
-     WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);;
-     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
-     return;
+  WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);;
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
+  return;
 }
